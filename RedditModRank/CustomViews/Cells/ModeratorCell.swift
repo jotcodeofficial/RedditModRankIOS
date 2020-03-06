37faf9c7c3cb74139503ace98f8e2c5f163320c9
BlockX <tokeninventory@gmail.com>
2020-03-07 00:11:02 +0100

    Added the moderator cell view

---
+++ RedditModRank/CustomViews/Cells/ModeratorCell.swift
@@ -0,0 +1,49 @@
+//
+//  ModeratorCell.swift
+//  RedditModRank
+//
+//  Created by Work on 06/03/2020.
+//  Copyright © 2020 JotCode. All rights reserved.
+//
+
+import UIKit
+
+class ModeratorCell: UICollectionViewCell {
+    
+    static let resuseID = "ModeratorCell"
+    let avatarImageView = MRAvatarImageView(frame: .zero)
+    let usernameLabel = MRTitleLabel(textAlignment: .center, fontSize: 16)
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func set(moderator: User) {
+        usernameLabel.text = moderator.name
+    }
+    
+    private func configure() {
+        addSubview(avatarImageView)
+        addSubview(usernameLabel)
+        
+        let padding: CGFloat = 8
+        
+        NSLayoutConstraint.activate([
+            avatarImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: padding),
+            avatarImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
+            avatarImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
+            avatarImageView.heightAnchor.constraint(equalTo: avatarImageView.widthAnchor),
+            
+            usernameLabel.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 12),
+            usernameLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
+            usernameLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
+            usernameLabel.heightAnchor.constraint(equalToConstant: 20)
+        ])
+    }
+    
+}
--- RedditModRank/Screens/ModListVC.swift
+++ RedditModRank/Screens/ModListVC.swift
@@ -13,68 +13,81 @@ class ModListVC: UIViewController {
     var subreddit: String!
     var simpleModerators: [Moderator] = []
     var finalModerators: [User] = []
+    var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBackground
-        navigationController?.navigationBar.prefersLargeTitles = true
-        
-    
-        
-        NetworkManager.shared.getModerators(for: subreddit) { result in
-            
-            switch result {
-                case .success(let moderators): do {
-                    if (moderators.count <= 0) {
-                                  self.presentMRAlertOnMainThread(title: "Whoops", message: ErrorMessage.noSubredditOrModerators.rawValue, buttonTitle: "Ok")
-                                  return
-                              }
-
-                              self.simpleModerators.append(contentsOf: moderators)
-                              
-                              self.simpleModerators.forEach { (moderator) in
-                                  NetworkManager.shared.getUser(for: moderator.name) { (result) in
-                    
-                                    switch result {
-                                        case .success(let user): do {
-                                            print("User\(user)")
-                                            self.finalModerators.append(user)
-                                            
-                                        }
-                                        case .failure(let error): do {
-                                            switch error {
-                                                case .userDoesNotExist: do {
-                                                    print("Error does not exist")
-                                                }
-                                                default: do {
-                                                      self.presentMRAlertOnMainThread(title: "Whoops", message: error.rawValue, buttonTitle: "Ok")
-                                                    }
-                                            }
-                                           
-                                        }
-                                    }
-
-                                  }
-                      
-                              }
-                }
-                case .failure(let error): do {
-                    self.presentMRAlertOnMainThread(title: "Whoops", message: error.rawValue, buttonTitle: "Ok")
-                }
-            }
-
-            
-            
-        }
-  
-
-        
+        configureViewController()
+        configureCollectionView()
+        getModerators()
         
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+    
+    
+    func configureViewController() {
+        view.backgroundColor = .systemBackground
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    func configureCollectionView() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewFlowLayout())
+        view.addSubview(collectionView)
+        collectionView.backgroundColor = .systemPink
+        collectionView.register(ModeratorCell.self, forCellWithReuseIdentifier: ModeratorCell.resuseID)
+    }
+    
+    func getModerators() {
+          NetworkManager.shared.getModerators(for: subreddit) { result in
+              
+              switch result {
+                  case .success(let moderators): do {
+                      if (moderators.count <= 0) {
+                                    self.presentMRAlertOnMainThread(title: "Whoops", message: ErrorMessage.noSubredditOrModerators.rawValue, buttonTitle: "Ok")
+                                    return
+                                }
+
+                                self.simpleModerators.append(contentsOf: moderators)
+                                
+                                self.simpleModerators.forEach { (moderator) in
+                                    NetworkManager.shared.getUser(for: moderator.name) { (result) in
+                      
+                                      switch result {
+                                          case .success(let user): do {
+                                              print("User\(user)")
+                                              self.finalModerators.append(user)
+                                              
+                                          }
+                                          case .failure(let error): do {
+                                              switch error {
+                                                  case .userDoesNotExist: do {
+                                                      print("Error does not exist")
+                                                  }
+                                                  default: do {
+                                                        self.presentMRAlertOnMainThread(title: "Whoops", message: error.rawValue, buttonTitle: "Ok")
+                                                      }
+                                              }
+                                             
+                                          }
+                                      }
+
+                                    }
+                        
+                                }
+                  }
+                  case .failure(let error): do {
+                      self.presentMRAlertOnMainThread(title: "Whoops", message: error.rawValue, buttonTitle: "Ok")
+                  }
+              }
+
+              
+              
+          }
+    
     }
     
 
